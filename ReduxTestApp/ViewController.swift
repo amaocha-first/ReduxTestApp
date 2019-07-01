8513cd1990de9058dcd407d9c352714364b2692f
NishizawaShota <nishizawa@emol.jp>
2019-07-01 17:22:10 +0900

    Initial Commit

---
+++ ReduxTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxTestApp
+//
+//  Created by coco j on 2019/07/01.
+//  Copyright © 2019 amaocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
